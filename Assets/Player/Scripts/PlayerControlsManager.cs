0183ec805cecce88ed297ff72064ef8a9916a61b
badihaki <jojackblack@gmail.com>
2024-07-04 20:47:18 -0400

    added all inputs to player input controller input action asset. Created OnMove() function to get move input

--- Assets/Player/Scripts/PlayerControlsManager.cs
+++ Assets/Player/Scripts/PlayerControlsManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControlsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [field: SerializeField] public Vector2 moveInput { get; private set; }
+    [field: SerializeField] public Vector2 lookInputKbm { get; private set; }
+    [field: SerializeField] public Vector2 lookInputGamepad { get; private set; }
+    [field: SerializeField] public bool shootInput { get; private set; }
+    [field: SerializeField] public bool meleeInput { get; private set; }
+    [field: SerializeField] public bool dashInput { get; private set; }
+    [field: SerializeField] public bool reloadInput { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        Vector2 input = context.ReadValue<Vector2>().normalized;
+        float x = Mathf.Round(input.x);
+        float y = Mathf.Round(input.y);
+        moveInput = new Vector2(x, y);
     }
 }
