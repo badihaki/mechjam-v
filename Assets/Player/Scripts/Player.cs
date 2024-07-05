c9c774720a914188401f858ac32b22893588d996
badihaki <jojackblack@gmail.com>
2024-07-04 20:57:41 -0400

    player is moving and rotating correctly. Next up is cam

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [field: SerializeField] public int playerID { get; private set; }
     [field: SerializeField] public PlayerControlsManager controls { get; private set; }
     [field: SerializeField] public PlayerLocomotionController locomotionController { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
+        controls = GetComponent<PlayerControlsManager>();
         
+        // locomotion
+        locomotionController = GetComponent<PlayerLocomotionController>();
+        locomotionController.InitializeController(this);
     }
 
     // Update is called once per frame
--- Assets/Player/Scripts/PlayerLocomotionController.cs
+++ Assets/Player/Scripts/PlayerLocomotionController.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLocomotionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Player player;
+    private Rigidbody physicsController;
+
+    private bool ready = false;
+    
+    public void InitializeController(Player _player)
     {
-        
+        player = _player;
+        physicsController = GetComponent<Rigidbody>();
+        ready = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ready)
+        {
+            ControlMovement();
+        }
+    }
+
+    private void ControlMovement()
+    {
+        Vector3 movement = new Vector3(player.controls.moveInput.x, 0.0f, player.controls.moveInput.y);
+
+        if(movement != Vector3.zero )
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.25f);
+        }
+
+        physicsController.velocity = movement;
     }
 }
